6030b8b8c233ecda245258c02287ee7f25410cb2
Dilek Ayşe Gün <58185677+DilekAyse@users.noreply.github.com>
2020-07-14 08:25:21 +0300

    Add files via upload

---
+++ tablo_olustur/tablo_olustur/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace tablo_olustur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TABLO_OLUSTUR = new System.Windows.Forms.Button();
+            this.DATABASE_ADRES = new System.Windows.Forms.TextBox();
+            this.MAKINE_ISMI = new System.Windows.Forms.TextBox();
+            this.MAKINA_1 = new System.Windows.Forms.TextBox();
+            this.recete_bilgisi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.veri_ekle = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.veri_sil = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TABLO_OLUSTUR
+            // 
+            this.TABLO_OLUSTUR.Location = new System.Drawing.Point(613, 12);
+            this.TABLO_OLUSTUR.Name = "TABLO_OLUSTUR";
+            this.TABLO_OLUSTUR.Size = new System.Drawing.Size(112, 23);
+            this.TABLO_OLUSTUR.TabIndex = 0;
+            this.TABLO_OLUSTUR.Text = "TABLO_OLUSTUR";
+            this.TABLO_OLUSTUR.UseVisualStyleBackColor = true;
+            this.TABLO_OLUSTUR.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DATABASE_ADRES
+            // 
+            this.DATABASE_ADRES.Location = new System.Drawing.Point(22, 12);
+            this.DATABASE_ADRES.Name = "DATABASE_ADRES";
+            this.DATABASE_ADRES.Size = new System.Drawing.Size(583, 20);
+            this.DATABASE_ADRES.TabIndex = 1;
+            this.DATABASE_ADRES.Text = "DILEKGUN\\SQLEXPRESS;Integrated Security=True";
+            this.DATABASE_ADRES.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // MAKINE_ISMI
+            // 
+            this.MAKINE_ISMI.Location = new System.Drawing.Point(22, 38);
+            this.MAKINE_ISMI.Name = "MAKINE_ISMI";
+            this.MAKINE_ISMI.Size = new System.Drawing.Size(583, 20);
+            this.MAKINE_ISMI.TabIndex = 2;
+            this.MAKINE_ISMI.Text = "Z_FORD_EF_";
+            // 
+            // MAKINA_1
+            // 
+            this.MAKINA_1.Location = new System.Drawing.Point(22, 73);
+            this.MAKINA_1.Name = "MAKINA_1";
+            this.MAKINA_1.Size = new System.Drawing.Size(252, 20);
+            this.MAKINA_1.TabIndex = 3;
+            this.MAKINA_1.Text = "MAKINA_1_KISA_YUZDE_YUZ";
+            // 
+            // recete_bilgisi
+            // 
+            this.recete_bilgisi.Location = new System.Drawing.Point(120, 123);
+            this.recete_bilgisi.Name = "recete_bilgisi";
+            this.recete_bilgisi.Size = new System.Drawing.Size(100, 20);
+            this.recete_bilgisi.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Reçete Bilgisi:";
+            // 
+            // veri_ekle
+            // 
+            this.veri_ekle.Location = new System.Drawing.Point(15, 203);
+            this.veri_ekle.Name = "veri_ekle";
+            this.veri_ekle.Size = new System.Drawing.Size(100, 23);
+            this.veri_ekle.TabIndex = 12;
+            this.veri_ekle.Text = "Veri Ekle";
+            this.veri_ekle.UseVisualStyleBackColor = true;
+            this.veri_ekle.Click += new System.EventHandler(this.veri_ekle_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Swıtch Acı Alt:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // veri_sil
+            // 
+            this.veri_sil.Location = new System.Drawing.Point(120, 203);
+            this.veri_sil.Name = "veri_sil";
+            this.veri_sil.Size = new System.Drawing.Size(97, 23);
+            this.veri_sil.TabIndex = 15;
+            this.veri_sil.Text = "Veri Sil";
+            this.veri_sil.UseVisualStyleBackColor = true;
+            this.veri_sil.Click += new System.EventHandler(this.veri_sil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 328);
+            this.Controls.Add(this.veri_sil);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.veri_ekle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recete_bilgisi);
+            this.Controls.Add(this.MAKINA_1);
+            this.Controls.Add(this.MAKINE_ISMI);
+            this.Controls.Add(this.DATABASE_ADRES);
+            this.Controls.Add(this.TABLO_OLUSTUR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TABLO_OLUSTUR;
+        private System.Windows.Forms.TextBox DATABASE_ADRES;
+        private System.Windows.Forms.TextBox MAKINE_ISMI;
+        private System.Windows.Forms.TextBox MAKINA_1;
+        private System.Windows.Forms.TextBox recete_bilgisi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button veri_ekle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button veri_sil;
+    }
+}
+
---
+++ tablo_olustur/tablo_olustur/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace tablo_olustur
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglan = new SqlConnection("Data Source=DILEKGUN\\SQLEXPRESS; Initial Catalog = ornek; Integrated Security=True");
+
+            try
+            {
+                baglan.Open();
+                //tablo oluşturduk
+                using (SqlCommand command = new SqlCommand("CREATE TABLE " + MAKINE_ISMI.Text + MAKINA_1.Text + "(" +
+
+                    "RECETE_BILGISI float," +
+
+                    "SWITCH_ACI_ALT float , SWITCH_ACI_OK float , SWITCH_ACI_UST float, SWITCH_ACI_OK_BIT varchar(MAX)," +
+                    "DISLI_KONTROL_ALT float, DISLI_KONTROL_OK float,DISLI_KONTROL_UST float, DISLI_KONTROL_OK_BIT varchar(MAX)," +
+                    "EFFICIENCY_ALT float, EFFICIENCY_OK float, EFFICIENCY_UST float , EFFICIENCY_OK_BIT varchar(MAX)," +
+                    "BUTON_MESAFESI_ALT float, BUTON_MESAFESI_OK float, BUTON_MESAFESI_UST float, BUTON_MESAFESI_OK_BIT varchar(MAX)," +
+                    "BUTON_KUVVET1_ALT float, BUTON_KUVVET1_OK float,BUTON_KUVVET1_UST float,BUTON_KUVVET1_OK_BIT varchar(MAX), " +
+                    "BUTON_KUVVET_2_ALT float, BUTON_KUVVET_2_OK float, BUTON_KUVVET_2_UST float, BUTON_KUVVET_2_OK_BIT varchar(MAX), " +
+                    "BUTON_BASMA_KURS_OK float, BUTON_BASMA_KURS_UST float, BUTON_BASMA_KURS_OK_BIT varchar(MAX), " +
+                    "YANAL_BOSLUK_OK float, YANAL_BOSLUK_UST float, YANAL_BOSLUK_OK_BIT varchar(MAX), " +
+                    "OK_ADET float, NOK_ADET float, " +
+                    "TARIH datetime);", baglan))
+
+                    command.ExecuteNonQuery();
+
+                baglan.Close();
+
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+           
+
+        }
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void veri_ekle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection baglan = new SqlConnection("Data Source=DILEKGUN\\SQLEXPRESS; Initial Catalog = ornek; Integrated Security=True");
+                baglan.Open();
+                string ekle = "insert into Z_FORD_EF_MAKINA_1_KISA_YUZDE_YUZ(RECETE_BILGISI,SWITCH_ACI_ALT,TARIH) values (@RECETE_BILGISI,@SWITCH_ACI_ALT,@TARIH)";
+                SqlCommand MAKINA_1_KISA_YUZDE_YUZ = new SqlCommand(ekle,baglan);
+
+                MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@RECETE_BILGISI", recete_bilgisi.Text);
+                MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@SWITCH_ACI_ALT", textBox1.Text);
+                MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@TARIH", DateTime.Now); 
+
+              
+
+                MAKINA_1_KISA_YUZDE_YUZ.ExecuteNonQuery();
+                
+                MessageBox.Show("Müşteri Kayıt İşlemi Gerçekleşti.");
+
+                baglan.Close();
+               
+
+
+                /*
+                  baglan.Open();  
+                  SqlCommand MAKINA_1_KISA_YUZDE_YUZ = new SqlCommand("insert into" + LogTut.makine_ismi + "MAKINA_1_KISA_YUZDE_YUZ (" +
+
+                  "RECETE_BILGISI, " + "TARIH,VardiyaAdi,Urun ) VALUES( " +
+                  " @RECETE_BILGISI, " + "@TARIH, @VardiyaAdi, @Urun )", baglan);
+
+                  MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@RECETE_BILGISI", 3.14);
+                  MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@TARIH", DateTime.Now );
+                  MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@VardiyaAdi", AktifVardiyaAdi );
+                  MAKINA_1_KISA_YUZDE_YUZ.Parameters.AddWithValue("@Urun",AktifReferansAdi );
+
+                  MAKINA_1_KISA_YUZDE_YUZ.ExecuteNonQuery();
+                  */
+            }
+
+            catch (Exception hata)
+            {
+                MessageBox.Show("İşlem Sırasında Hata Oluştu." + hata.Message);
+            }
+
+
+
+        }
+
+        private void veri_sil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection baglan = new SqlConnection("Data Source=DILEKGUN\\SQLEXPRESS; Initial Catalog = ornek; Integrated Security=True");
+                baglan.Open();
+                string secmeSorgusu = "SELECT * from Z_FORD_EF_MAKINA_1_KISA_YUZDE_YUZ where RECETE_BILGISI=@RECETE_BILGISI";
+                SqlCommand secmeKomutu = new SqlCommand(secmeSorgusu, baglan);
+                secmeKomutu.Parameters.AddWithValue("@RECETE_BILGISI", recete_bilgisi.Text);
+                SqlDataAdapter da = new SqlDataAdapter(secmeKomutu);
+                SqlDataReader dr = secmeKomutu.ExecuteReader();
+                if (dr.Read()) //Datareader herhangi bir okuma yapabiliyorsa aşağıdaki kodlar çalışır.
+                {
+                    string recete_bilgisii = dr["RECETE_BILGISI"].ToString();
+                    dr.Close();
+                    //Datareader ile okunan reçete bilgisini recete_bilgisii değişkenine atadım.
+                    //Datareader açık olduğu sürece başka bir sorgu çalıştıramayacağımız için dr nesnesini kapatıyoruz.
+                    DialogResult durum = MessageBox.Show(recete_bilgisii + " kaydını silmek istediğinizden emin misiniz?", "Silme Onayı", MessageBoxButtons.YesNo);
+                    //Kullanıcıya silme onay sorusu
+                    if (DialogResult.Yes == durum) // Eğer kullanıcı Evet seçeneğini seçmişse, veritabanından kaydı silecek kodlar çalışır.
+                    {
+                        string silmeSorgusu = "DELETE from Z_FORD_EF_MAKINA_1_KISA_YUZDE_YUZ where RECETE_BILGISI=@RECETE_BILGISI";
+                        //RECETE_BILGISI parametresine bağlı olarak müşteri kaydını silen sql sorgusu
+                        SqlCommand silKomutu = new SqlCommand(silmeSorgusu, baglan);
+                        silKomutu.Parameters.AddWithValue("@RECETE_BILGISI", recete_bilgisi.Text);
+                        silKomutu.ExecuteNonQuery();
+                        MessageBox.Show("Kayıt Silindi...");
+                        //Silme işlemini gerçekleştirdikten sonra kullanıcıya mesaj verdik.
+                    }
+                }
+
+                MessageBox.Show("Müşteri Kayıt İşlemi Gerçekleşti.");
+
+                baglan.Close();
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("İşlem Sırasında Hata Oluştu." + hata.Message);
+            }
+        }
+     }
+}
